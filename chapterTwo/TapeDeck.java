7179f4cb32b778d80a3813a10dd63dae6e69f461
Michael <mikhail6886@yandex.ru>
2024-10-09 20:15:26 +0500

    added the new class TapeDeck

---
+++ chapterTwo/TapeDeck.java
@@ -0,0 +1,11 @@
+class TapeDeck {
+	boolean canRecord = false;
+
+	void playTape() {
+		System.out.println("пленка проигрывается");
+	}
+
+	void recordTape() {
+		System.out.println("идёт запись на пленку");
+	}
+}
