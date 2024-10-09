b0bebd4e11827674e28ca7510a2c1182fd263034
Michael <mikhail6886@yandex.ru>
2024-10-09 21:43:11 +0500

    added the new class DVDPlayer

---
+++ chapterTwo/DVDPlayer.java
@@ -0,0 +1,12 @@
+class DVDPlayer {
+	
+	boolean canRecord = false;
+
+	void recordDVD() {
+		System.out.println("идет запись DVD");
+	}
+
+	void playDVD() {
+		System.out.println("DVD проигрывается");
+	}
+}
