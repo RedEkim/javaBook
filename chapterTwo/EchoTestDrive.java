f59b9aea87aaa984eea74fbca6fd19d3124968b3
Michael <mikhail6886@yandex.ru>
2024-10-10 20:32:46 +0500

    added the new class EchoTestDrive

--- chapterTwo/EchoTestDrive.java
+++ chapterTwo/EchoTestDrive.java
@@ -6,15 +6,15 @@ public class EchoTestDrive {
 		Echo e2 = new Echo();
 
 		int x = 0;
-		while (/*two*/ x < 5 ) {
+		while (/*two*/ x < 4 ) {
 			e1.hello();
 			// three
 			e1.count = e1.count + 1;
-			if (/**/ x < 5) {
+			if (/**/ x == 3) {
 				e2.count = e2.count + 1;
 			}
 
-			if (/**/ x == 4) {
+			if (/**/ x > 0) {
 				e2.count = e2.count + e1.count;
 			}
 			x = x + 1;
