9f4d4812196337ab7b9809aae7224422b62d43f1
Michael <mikhail6886@yandex.ru>
2024-10-11 17:43:55 +0500

    update class Books

--- chapterThree/BooksTestDrive.java
+++ chapterThree/BooksTestDrive.java
@@ -1,7 +1,12 @@
 class BooksTestDrive {
 	public static void main(String[] args) {
-		Books [] myBooks = new Book[3];
+		Books[] myBooks = new Books[3];
 		int x = 0;
+
+		myBooks[0] = new Books();
+		myBooks[1] = new Books();
+		myBooks[2] = new Books();
+
 		myBooks[0].title = "Плоды Java";
 		myBooks[1].title = "Java Гэтсби";
 		myBooks[2].title = "Сборник рецептов по Java";
@@ -12,8 +17,8 @@ class BooksTestDrive {
 
 		while (x < 3) {
 			System.out.print(myBooks[x].title);
-			System.out.print(", автор");
-			System,out.print(myBooks[x].author);
+			System.out.print(", автор ");
+			System.out.print(myBooks[x].author);
 			x = x + 1;
 		}
 	}
