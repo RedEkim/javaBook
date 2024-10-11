eaa783d3275569620d8808c3b57ce3fcb5819585
Michael <mikhail6886@yandex.ru>
2024-10-11 17:56:36 +0500

    added the new class Hobbits

---
+++ chapterThree/Hobbits.java
@@ -0,0 +1,26 @@
+class Hobbits {
+	String name;
+	public static void main(String[] args) {
+		Hobbits [] h = new Hobbits[3];
+		// h[0] = new Hobbits();
+		// h[1] = new Hobbits();
+		// h[2] = new Hobbits();
+		
+		int z = -1;
+
+		while(z < 2) {
+			z = z + 1;
+			h[z] = new Hobbits();
+			h[z].name = "Бильбо";
+			
+			if (z == 1) {
+				h[z].name = "Фродо";
+			}
+			if (z == 2) {
+				h[z].name = "Сэм";
+			}
+			System.out.print(h[z].name + " - ");
+			System.out.println("имя хорошего хоббита");
+		}
+	}
+}
